7a46088d7524711361960b32416b6b4b1eef9ad2
Aravind Balaji <balajiaravind99@gmail.com>
2025-08-07 22:47:07 +0530

    check-1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const dotenv = require('dotenv');
 dotenv.config();    
 const taskRoutes = require('./routes/taskRoutes.js');
 const app = express();
-
+  
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.json({ status: true, version: 1 })
+  res.json({ status: true, version: 2 })
 });
 
 module.exports = router;

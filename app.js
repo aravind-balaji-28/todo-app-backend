09ae61d203815d8e37d77fa2c9c221bee46beba0
Aravind Balaji <101121374+aravind-balaji-28@users.noreply.github.com>
2025-08-31 01:50:46 +0530

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected!!!'))
+}).then(() => console.log('MongoDB connected!'))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT 
